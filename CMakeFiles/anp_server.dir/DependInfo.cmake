484898eca1c4ff64d64506a977d7879f17c28916
daan-fh <113973927+daan-fh@users.noreply.github.com>
2024-09-20 17:00:01 +0200

    Add files via upload

---
+++ CMakeFiles/anp_server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/daan1809/ANPProject/anp-2024-skeleton-main/server-client/common.c" "/home/daan1809/ANPProject/anp-2024-skeleton-main/CMakeFiles/anp_server.dir/server-client/common.c.o"
+  "/home/daan1809/ANPProject/anp-2024-skeleton-main/server-client/tcp_server.c" "/home/daan1809/ANPProject/anp-2024-skeleton-main/CMakeFiles/anp_server.dir/server-client/tcp_server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/anp_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anp_server.dir/server-client/common.c.o"
+  "CMakeFiles/anp_server.dir/server-client/tcp_server.c.o"
+  "build/anp_server"
+  "build/anp_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/anp_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
